05e0d0f91750c403f7c4101ff25796d63cbc0b70
Amir Teymuri <ateymuri63@gmail.com>
2022-12-16 16:10:12 +0100

    add test function for running in repl

--- utils.py
+++ utils.py
@@ -7,9 +7,9 @@ import asyncio
 from rtmidi.midiconstants import NOTE_OFF, NOTE_ON
 
 
-MIDIOUT = rtmidi.MidiOut()
+MIDI_OUT = rtmidi.MidiOut()
 
-def play_note(note=60, dur=1, ch=1,vel=127,  out=MIDIOUT):
+def play_note(note=60, dur=1, ch=1,vel=127,  out=MIDI_OUT):
     # 3 bytes of non,nof msgs
     note_on = [NOTE_ON + ch - 1, note, vel]
     note_off = [NOTE_OFF + ch - 1, note, vel]
@@ -19,7 +19,7 @@ def play_note(note=60, dur=1, ch=1,vel=127,  out=MIDIOUT):
     finally:
         out.send_message(note_off)
 
-def play_chord(notes=[60], dur=1, ch=1,vel=127, out=MIDIOUT):
+def play_chord(notes=[60], dur=1, ch=1,vel=127, out=MIDI_OUT):
     count = len(notes)
     on_msgs = [[NOTE_ON + ch - 1, n, vel] for n in notes]
     off_msgs = [[NOTE_OFF + ch - 1, n, vel] for n in notes]
@@ -35,7 +35,7 @@ def play_chord(notes=[60], dur=1, ch=1,vel=127, out=MIDIOUT):
 async def _play_voice(pitches, durs, ch, vels, out, show):
     for i, p in enumerate(pitches):
         if show:
-            print(f"CH {ch} pitch {p} dur {durs[i]} vel {vels[i]}")
+            print(f"Ch {ch} Pitch {p} Dur {durs[i]} Vel {vels[i]}")
         if p < 0: # rest
             non = [NOTE_ON + ch -1, 0, 0]
             nof = [NOTE_OFF + ch - 1, 0, 0]
@@ -48,7 +48,7 @@ async def _play_voice(pitches, durs, ch, vels, out, show):
         finally:
             out.send_message(nof)
 
-async def play_poly(voice_pitches, voice_durs, chs, voice_vels, show=False, out=MIDIOUT):
+async def play_poly(voice_pitches, voice_durs, chs, voice_vels, show=False, out=MIDI_OUT):
     play_voices = [] # playable voices
     for i in range(len(voice_pitches)):
         play_voices.append(
@@ -64,32 +64,32 @@ async def play_poly(voice_pitches, voice_durs, chs, voice_vels, show=False, out=
     await asyncio.wait(play_voices)
 
 
-if __name__ == "__main__":
-    with (MIDIOUT.open_port(0) if MIDIOUT.get_ports() else
-            MIDIOUT.open_virtual_port("My virtual output")):
+def test():
+    global MIDI_OUT
+    with (MIDI_OUT.open_port(0) if MIDI_OUT.get_ports() else
+            MIDI_OUT.open_virtual_port("My virtual output")):
 
-        # import random
+        import random
+
         # chs = [[64+i for i in [0, 4, 7, 12]] for _ in range(10)]
         # for i in range(1000):
         #     for ch in chs:
         #         print(i, ch)
         #         play_chord(ch, dur=.0001+(i*.001), vel=100, ch=1)
 
-
-
+        v_cnt = 4
+        n_cnt = 1000
         asyncio.run(
             play_poly(
-                [[50, -1, 60, -1, 70, -1, 40],
-                [50, -1, 60, -1, 70, -1, 40]],
-                [[1, 1, .5, 1, .25, .75, 1], 
-                [.5, 1, .5, 1, .25, .75, 1]],
-                [1, 2], 
-                [[100, 100, 100, 100, 100, 100, 100],
-                [100, 100, 100, 100, 100, 100, 100]],
+                [[random.randint(48, 82) for _ in range(n_cnt)] for _ in range(v_cnt)],
+                [[.001] * n_cnt for _ in range(v_cnt)],
+                [random.randint(1, 16) for _ in range(v_cnt)], 
+                [[50] * n_cnt for _ in range(v_cnt)],
                 True
             )
         )
 
-    del MIDIOUT
+    del MIDI_OUT
 
-
+if __name__ == "__main__":
+    test()

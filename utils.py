e458f9bef58b4a45bc352831af71097bf5bea014
Amir Teymuri <ateymuri63@gmail.com>
2022-12-16 08:36:43 +0100

    Support rest with negative numbers, add print opt

--- utils.py
+++ utils.py
@@ -32,17 +32,23 @@ def play_chord(notes=[60], dur=1, ch=1,vel=127, out=MIDIOUT):
             out.send_message(off_msgs[i])
 
 
-async def _play_voice(pitches, durs, ch, vels, out):
+async def _play_voice(pitches, durs, ch, vels, out, show):
     for i, p in enumerate(pitches):
-        non = [NOTE_ON + ch - 1, p, vels[i]]
-        nof = [NOTE_OFF + ch - 1, p, vels[i]]
+        if show:
+            print(f"CH {ch} pitch {p} dur {durs[i]} vel {vels[i]}")
+        if p < 0: # rest
+            non = [NOTE_ON + ch -1, 0, 0]
+            nof = [NOTE_OFF + ch - 1, 0, 0]
+        else:
+            non = [NOTE_ON + ch - 1, p, vels[i]]
+            nof = [NOTE_OFF + ch - 1, p, vels[i]]
         try:
             out.send_message(non)
             await asyncio.sleep(durs[i])
         finally:
             out.send_message(nof)
 
-async def play_poly(voice_pitches, voice_durs, chs, voice_vels, out=MIDIOUT):
+async def play_poly(voice_pitches, voice_durs, chs, voice_vels, show=False, out=MIDIOUT):
     play_voices = [] # playable voices
     for i in range(len(voice_pitches)):
         play_voices.append(
@@ -51,7 +57,8 @@ async def play_poly(voice_pitches, voice_durs, chs, voice_vels, out=MIDIOUT):
                 voice_durs[i],
                 chs[i],
                 voice_vels[i],
-                out)
+                out,
+                show)
             )
         )
     await asyncio.wait(play_voices)
@@ -69,14 +76,19 @@ if __name__ == "__main__":
         #         play_chord(ch, dur=.0001+(i*.001), vel=100, ch=1)
 
 
-        asyncio.run(play_poly(
-            [list(range(40, 50)),
-            list(range(40, 60)),
-            list(range(40, 70))],
-            [[1] * 10, [.5] * 20, [1/3] * 30],
-            [1, 2, 3],
-            [[100] * 10, [100] * 20, [100] * 30]
-        ))
+
+        asyncio.run(
+            play_poly(
+                [[50, -1, 60, -1, 70, -1, 40],
+                [50, -1, 60, -1, 70, -1, 40]],
+                [[1, 1, .5, 1, .25, .75, 1], 
+                [.5, 1, .5, 1, .25, .75, 1]],
+                [1, 2], 
+                [[100, 100, 100, 100, 100, 100, 100],
+                [100, 100, 100, 100, 100, 100, 100]],
+                True
+            )
+        )
 
     del MIDIOUT
 

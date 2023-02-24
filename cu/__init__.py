d0b01778a3db9426cc819a214ccd5db94a659799
Amir Teymuri <amiratwork22@gmail.com>
2023-02-24 12:29:25 +0100

    Add opt param to processor
    There are situations where I first want to listen to the result and
    decide whether I want to keep it or not afterwards.

--- cu/__init__.py
+++ cu/__init__.py
@@ -12,9 +12,17 @@ except ImportError: # running from shell
     _SCRIPT = False
 
 
-def proc(events, mid=""):
-    """Processes every thing!"""
+def proc(events, mid="", opt=False):
+    """Processes every thing!
+    Set opt to True to listen first and decide to write to the disk or not afterwards."""
     if mid: # write to a midi file
         mu.proc(events, mid)
     else: # play now
-        asyncio.run(rt.proc(events, _SCRIPT))
+        if opt:
+            asyncio.run(rt.proc(events, _SCRIPT))
+            mid_path = input("Specify path to write\n")
+            if mid_path:
+                mu.proc(events, mid_path)
+                print(f"Wrote to {mid_path}")
+        else:
+            asyncio.run(rt.proc(events, _SCRIPT))

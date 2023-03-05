0d1e183be34f217714c742737bc411b0c789ac81
Amir Teymuri <amiratwork22@gmail.com>
2023-03-05 17:52:22 +0100

    Rename scale_to_sum -> normsum (as in supercollider)

--- computil/common.py
+++ computil/common.py
@@ -279,12 +279,12 @@ def onset_to_dur(onsets):
     onsets = list(onsets)
     return [b - a for b, a in zip(onsets[1:], onsets[:-1])]
 
-def scale_to_sum(nums, _sum):
+def normsum(ns, _sum=1):
     """Scales a set of numbers such that they sum up 
     to _sum after scaling
 
     https://math.stackexchange.com/questions/1009138/how-do-you-scale-a-set-of-number-such-that-they-sum-to-0-5-after-scaling"""
-    return [n * _sum / sum(nums) for n in nums]
+    return [n * _sum / sum(ns) for n in ns]
 
 def minmax_norm(x, minx, maxx, low_bound=0, up_bound=1):
     """Min-max normalization (usually called feature scaling) performs 

80ea448909fce94589c3522346338262817d3b2f
Amir Teymuri <amiratwork22@gmail.com>
2023-03-14 23:39:02 +0100

    Rename offset -> initos for initial onset

--- computil/common.py
+++ computil/common.py
@@ -277,15 +277,15 @@ def get_intervals(ns):
     return [b - a for a, b in zip(ns[:-1], ns[1:])]
 
 
-def dur_to_onset(durs, offset=0):
+def dur_to_onset(durs, initos=0):
     """Returns a list of (accumulated onset, corresponding duration).
-    Offset is the desired starting onset."""
+    The initial onset is the desired starting onset."""
     durs = list(durs) # convert to pop
     onsets = []
     while durs:
         d = durs.pop(0)
-        onsets.append((d, offset))
-        offset += d
+        onsets.append((d, initos))
+        initos += d
     return onsets
 
 def onset_to_dur(onsets):
